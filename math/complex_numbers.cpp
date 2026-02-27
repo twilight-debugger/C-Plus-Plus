00b97feabccb9e522b50e14a8e71d7ada74e40e3
HARSH VERMA <tf141classified@gmail.com>
2026-02-27 23:53:45 +0530

    Rename members and update Complex class methods
    Refactor Complex class to improve naming conventions and structure.

--- math/complex_numbers.cpp
+++ math/complex_numbers.cpp
@@ -18,252 +18,218 @@
  * \brief Class Complex to represent complex numbers as a field.
  */
 class Complex {
-    // The real value of the complex number
-    double re;
-    // The imaginary value of the complex number
-    double im;
+    // Real part of the complex number
+    double real;
+    // Imaginary part of the complex number
+    double imaginary;
 
- public:
+public:
     /**
-     * \brief Complex Constructor which initialises our complex number.
-     * \details
-     * Complex Constructor which initialises the complex number which takes
-     * three arguments.
-     * @param x If the third parameter is 'true' then this x is the absolute
-     * value of the complex number, if the third parameter is 'false' then this
-     * x is the real value of the complex number (optional).
-     * @param y If the third parameter is 'true' then this y is the argument of
-     * the complex number, if the third parameter is 'false' then this y is the
-     * imaginary value of the complex number (optional).
-     * @param is_polar 'false' by default. If we want to initialise our complex
-     * number using polar form then set this to true, otherwise set it to false
-     * to use initialiser which initialises real and imaginary values using the
-     * first two parameters (optional).
+     * \brief Complex Constructor
+     *
+     * If is_polar == false:
+     *   x -> real part, y -> imaginary part
+     *
+     * If is_polar == true:
+     *   x -> magnitude, y -> argument (radians)
      */
-    explicit Complex(double x = 0.f, double y = 0.f, bool is_polar = false) {
+    explicit Complex(double x = 0.0, double y = 0.0, bool is_polar = false) {
         if (!is_polar) {
-            re = x;
-            im = y;
-            return;
+            real = x;
+            imaginary = y;
+        } else {
+            // Convert polar form to rectangular form
+            real = x * std::cos(y);
+            imaginary = x * std::sin(y);
         }
-
-        re = x * std::cos(y);
-        im = x * std::sin(y);
     }
 
     /**
-     * \brief Copy Constructor
-     * @param other The other number to equate our number to.
+     * \brief Copy constructor
      */
-    Complex(const Complex &other) : re(other.real()), im(other.imag()) {}
+    Complex(const Complex &other)
+        : real(other.real_()), imaginary(other.imag_()) {}
 
     /**
-     * \brief Member function to get real value of our complex number.
-     * Member function (getter) to access the class' re value.
+     * \brief Getter for real part
      */
-    double real() const { return this->re; }
+    double real_() const { return real; }
 
     /**
-     * \brief Member function to get imaginary value of our complex number.
-     * Member function (getter) to access the class' im value.
+     * \brief Getter for imaginary part
      */
-    double imag() const { return this->im; }
+    double imag_() const { return imaginary; }
 
     /**
-     * \brief Member function to give the modulus of our complex number.
-     * Member function to which gives the absolute value (modulus) of our
-     * complex number
-     * @return \f$ \sqrt{z \bar{z}} \f$ where \f$ z \f$ is our complex
-     * number.
+     * \brief Returns modulus (absolute value) of complex number
      */
     double abs() const {
-        return std::sqrt(this->re * this->re + this->im * this->im);
+        return std::sqrt(real * real + imaginary * imaginary);
     }
 
     /**
-     * \brief Member function to give the argument of our complex number.
-     * @return Argument of our Complex number in radians.
+     * \brief Returns argument (angle) in radians
      */
-    double arg() const { return std::atan2(this->im, this->re); }
-
-    /**
-     * \brief Operator overload of '+' on Complex class.
-     * Operator overload to be able to add two complex numbers.
-     * @param other The other number that is added to the current number.
-     * @return result current number plus other number
-     */
-    Complex operator+(const Complex &other) {
-        Complex result(this->re + other.re, this->im + other.im);
-        return result;
+    double arg() const {
+        return std::atan2(imaginary, real);
     }
 
     /**
-     * \brief Operator overload of '-' on Complex class.
-     * Operator overload to be able to subtract two complex numbers.
-     * @param other The other number being subtracted from the current number.
-     * @return result current number subtract other number
+     * \brief Addition operator
      */
-    Complex operator-(const Complex &other) {
-        Complex result(this->re - other.re, this->im - other.im);
-        return result;
+    Complex operator+(const Complex &other) const {
+        return Complex(real + other.real_, imaginary + other.imaginary);
     }
 
     /**
-     * \brief Operator overload of '*' on Complex class.
-     * Operator overload to be able to multiple two complex numbers.
-     * @param other The other number to multiply the current number to.
-     * @return result current number times other number.
+     * \brief Subtraction operator
      */
-    Complex operator*(const Complex &other) {
-        Complex result(this->re * other.re - this->im * other.im,
-                       this->re * other.im + this->im * other.re);
-        return result;
+    Complex operator-(const Complex &other) const {
+        return Complex(real - other.real_, imaginary - other.imaginary);
     }
 
     /**
-     * \brief Operator overload of '~' on Complex class.
-     * Operator overload of the BITWISE NOT which gives us the conjugate of our
-     * complex number. NOTE: This is overloading the BITWISE operator but its
-     * not a BITWISE operation in this definition.
-     * @return result The conjugate of our complex number.
+     * \brief Multiplication operator
+     */
+    Complex operator*(const Complex &other) const {
+        return Complex(
+            real * other.real_ - imaginary * other.imaginary,
+            real * other.imaginary + imaginary * other.real_
+        );
+    }
+
+    /**
+     * \brief Conjugate operator (overloads bitwise NOT ~)
      */
     Complex operator~() const {
-        Complex result(this->re, -(this->im));
-        return result;
+        return Complex(real, -imaginary);
     }
 
     /**
-     * \brief Operator overload of '/' on Complex class.
-     * Operator overload to be able to divide two complex numbers. This function
-     * would throw an exception if the other number is zero.
-     * @param other The other number we divide our number by.
-     * @return result Current number divided by other number.
+     * \brief Division operator
+     * \throws std::invalid_argument if dividing by zero
      */
-    Complex operator/(const Complex &other) {
-        Complex result = *this * ~other;
+    Complex operator/(const Complex &other) const {
+        // Compute denominator: |other|^2
         double denominator =
-            other.real() * other.real() + other.imag() * other.imag();
-        if (denominator != 0) {
-            result = Complex(result.real() / denominator,
-                             result.imag() / denominator);
-            return result;
-        } else {
-            throw std::invalid_argument("Undefined Value");
+            other.real_ * other.real_ + other.imaginary * other.imaginary;
+
+        if (denominator == 0.0) {
+            throw std::invalid_argument("Division by zero complex number");
         }
+
+        // Multiply by conjugate and divide by denominator
+        Complex numerator = (*this) * ~other;
+        return Complex(numerator.real_() / denominator,
+                       numerator.imag_() / denominator);
     }
 
     /**
-     * \brief Operator overload of '=' on Complex class.
-     * Operator overload to be able to copy RHS instance of Complex to LHS
-     * instance of Complex
+     * \brief Assignment operator
      */
     const Complex &operator=(const Complex &other) {
-        this->re = other.real();
-        this->im = other.imag();
+        real = other.real_();
+        imaginary = other.imag_();
         return *this;
     }
 };
 
 /**
- * \brief Operator overload of '==' on Complex class.
- * Logical Equal overload for our Complex class.
- * @param a Left hand side of our expression
- * @param b Right hand side of our expression
- * @return 'True' If real and imaginary parts of a and b are same
- * @return 'False' Otherwise.
+ * \brief Equality operator
  */
 bool operator==(const Complex &a, const Complex &b) {
-    return a.real() == b.real() && a.imag() == b.imag();
+    return a.real_() == b.real_() && a.imag_() == b.imag_();
 }
 
 /**
- * \brief Operator overload of '<<' of ostream for Complex class.
- * Overloaded insersion operator to accommodate the printing of our complex
- * number in their standard form.
- * @param os The console stream
- * @param num The complex number.
+ * \brief Stream insertion operator
  */
 std::ostream &operator<<(std::ostream &os, const Complex &num) {
-    os << "(" << num.real();
-    if (num.imag() < 0) {
-        os << " - " << -num.imag();
+    os << "(" << num.real_();
+    if (num.imag_() < 0) {
+        os << " - " << -num.imag_();
     } else {
-        os << " + " << num.imag();
+        os << " + " << num.imag_();
     }
     os << "i)";
     return os;
 }
 
 /**
- * \brief Function to get random numbers to generate our complex numbers for
- * test
+ * \brief Generates a random number in range [-0.5, 0.5]
  */
-double get_rand() { return (std::rand() % 100 - 50) / 100.f; }
-
-/**
- * Tests Function
- */
-void tests() {
-    std::srand(std::time(nullptr));
-    double x1 = get_rand(), y1 = get_rand(), x2 = get_rand(), y2 = get_rand();
-    Complex num1(x1, y1), num2(x2, y2);
-    std::complex<double> cnum1(x1, y1), cnum2(x2, y2);
-    Complex result;
-    std::complex<double> expected;
-    // Test for addition
-    result = num1 + num2;
-    expected = cnum1 + cnum2;
-    assert(((void)"1 + 1i + 1 + 1i is equal to 2 + 2i but the addition doesn't "
-                  "add up \n",
-            (result.real() == expected.real() &&
-             result.imag() == expected.imag())));
-    std::cout << "First test passes." << std::endl;
-    // Test for subtraction
-    result = num1 - num2;
-    expected = cnum1 - cnum2;
-    assert(((void)"1 + 1i - 1 - 1i is equal to 0 but the program says "
-                  "otherwise. \n",
-            (result.real() == expected.real() &&
-             result.imag() == expected.imag())));
-    std::cout << "Second test passes." << std::endl;
-    // Test for multiplication
-    result = num1 * num2;
-    expected = cnum1 * cnum2;
-    assert(((void)"(1 + 1i) * (1 + 1i) is equal to 2i but the program says "
-                  "otherwise. \n",
-            (result.real() == expected.real() &&
-             result.imag() == expected.imag())));
-    std::cout << "Third test passes." << std::endl;
-    // Test for division
-    result = num1 / num2;
-    expected = cnum1 / cnum2;
-    assert(((void)"(1 + 1i) / (1 + 1i) is equal to 1 but the program says "
-                  "otherwise.\n",
-            (result.real() == expected.real() &&
-             result.imag() == expected.imag())));
-    std::cout << "Fourth test passes." << std::endl;
-    // Test for conjugates
-    result = ~num1;
-    expected = std::conj(cnum1);
-    assert(((void)"(1 + 1i) has a conjugate which is equal to (1 - 1i) but the "
-                  "program says otherwise.\n",
-            (result.real() == expected.real() &&
-             result.imag() == expected.imag())));
-    std::cout << "Fifth test passes.\n";
-    // Test for Argument of our complex number
-    assert(((void)"(1 + 1i) has argument PI / 4 but the program differs from "
-                  "the std::complex result.\n",
-            (num1.arg() == std::arg(cnum1))));
-    std::cout << "Sixth test passes.\n";
-    // Test for absolute value of our complex number
-    assert(((void)"(1 + 1i) has absolute value sqrt(2) but the program differs "
-                  "from the std::complex result. \n",
-            (num1.abs() == std::abs(cnum1))));
-    std::cout << "Seventh test passes.\n";
+double get_rand() {
+    return (std::rand() % 100 - 50) / 100.0;
 }
 
 /**
- * Main function
+ * \brief Test suite
+ */
+void tests() {
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    double x1 = get_rand(), y1 = get_rand();
+    double x2 = get_rand(), y2 = get_rand();
+
+    Complex num1(x1, y1), num2(x2, y2);
+    std::complex<double> cnum1(x1, y1), cnum2(x2, y2);
+
+    // Lambda for floating-point comparison
+    auto close = [](double a, double b) {
+        return std::abs(a - b) < 1e-9;
+    };
+
+    Complex result;
+    std::complex<double> expected;
+
+    // Addition
+    result = num1 + num2;
+    expected = cnum1 + cnum2;
+    assert(close(result.real_(), expected.real()));
+    assert(close(result.imag_(), expected.imag()));
+    std::cout << "Addition test passed\n";
+
+    // Subtraction
+    result = num1 - num2;
+    expected = cnum1 - cnum2;
+    assert(close(result.real_(), expected.real()));
+    assert(close(result.imag_(), expected.imag()));
+    std::cout << "Subtraction test passed\n";
+
+    // Multiplication
+    result = num1 * num2;
+    expected = cnum1 * cnum2;
+    assert(close(result.real_(), expected.real()));
+    assert(close(result.imag_(), expected.imag()));
+    std::cout << "Multiplication test passed\n";
+
+    // Division
+    result = num1 / num2;
+    expected = cnum1 / cnum2;
+    assert(close(result.real_(), expected.real()));
+    assert(close(result.imag_(), expected.imag()));
+    std::cout << "Division test passed\n";
+
+    // Conjugate
+    result = ~num1;
+    expected = std::conj(cnum1);
+    assert(close(result.real_(), expected.real()));
+    assert(close(result.imag_(), expected.imag()));
+    std::cout << "Conjugate test passed\n";
+
+    // Argument
+    assert(close(num1.arg(), std::arg(cnum1)));
+    std::cout << "Argument test passed\n";
+
+    // Absolute value
+    assert(close(num1.abs(), std::abs(cnum1)));
+    std::cout << "Absolute value test passed\n";
+
+    std::cout << "\nAll tests passed successfully \n";
+}
+
+/**
+ * \brief Main function
  */
 int main() {
     tests();
